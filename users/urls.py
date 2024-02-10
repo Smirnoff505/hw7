d77b24ad6fff63923b9521bba3be921551a5b3d7
Oleg Smirnov <oleg.smirnoff505@yandex.ru>
2024-02-11 01:02:33 +0500

    added files

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,10 @@
+from django.urls import path
+
 from users.apps import UsersConfig
+from users.views import UserUpdateAPIView
 
 app_name = UsersConfig.name
 
+urlpatterns = [
+    path('user/update/<int:pk>', UserUpdateAPIView.as_view(), name='user-update'),
+]
